c1b616dec03b578a25dcd56928ab6985e8917aa0
Wzard <oncwnuGDYYREQp2D-dUhLySgDJOc@git.weixin.qq.com>
2020-05-19 18:12:06 +0800

    商品添加页面编写1/3

---
+++ miniprogram/components/card/index.js
@@ -0,0 +1,30 @@
+// components/card/index.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		name:{
+			type:String,
+			value:""
+		},
+		imageUrl:{
+			type:String,
+			value:""
+		}
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+
+	}
+})
